8d9f50832b57e947fe34a211c1155a5c8e5172eb
Sofiane NOUNOU <sofiane.nounou@ynov.com>
2023-07-07 20:38:02 +0100

    commit4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ const Home: NextPage = (props) => {
   return (
     <div>
       <Head>
-        <title>Solana Scaffold</title>
+        <title>PIXSELLS MISSIONS</title>
         <meta
           name="description"
-          content="Solana Scaffold"
+          content="PIXSELLS MISSIONS"
         />
       </Head>
       <HomeView />
